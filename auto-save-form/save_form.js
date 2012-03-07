fa157795781ef9d7b6fb9afe0ec4b93619f57f48
Sachin <sachingupta006@gmail.com>
2012-03-08 00:37:28 +0530

    README and comments

--- auto-save-form/save_form.js
+++ auto-save-form/save_form.js
@@ -1,10 +1,12 @@
   $(function() {
 
+    // My form uses a textarea that has TinyMCE editor applied to it
     var saveTimePeriod = 10000;
 
 
 	$(".save_button").click(function() 
 	{			
+        // save the contents of the editor instance to the underlying text area
 		var ed = tinymce.activeEditor; 
 		ed.save();
 		var link = $(this).attr("href");
@@ -15,6 +17,9 @@
 		$.ajax({
 		  type: "POST",
 		  url: link,
+          // I needed to exclude a hidden field named csrfmiddlwaretoken
+          // if you don't have any such requirement
+          // you can directly use $('#form").serialize()
 		  data: $("#form :input[name!='csrfmiddlewaretoken']").serialize(),
 		  dataType: 'json',
           success: function(){
@@ -26,22 +31,24 @@
 		return false;	
 	});
 
-    /* Makes the button unclickable when the blog is being saved */
+    /* Makes the button unclickable when the form is being saved */
     $('a.unclickable').click(function(){
         return false;
     });
 
-    /* Function to be called for saving the blog 
+    /* Function to be called for saving the form 
      * Can't use isDirty for 2 reasons
-     * First there are other fields in the blog that maybe changed
+     * First there are other fields in the form that maybe changed
      * Second once a change has been made the isDirty() function always remains true
      * will have to think of something other in future
      */
 
     function saveForm(){
 
+        // save the contents of the editor instance
     	var ed = tinymce.activeEditor; 
 	    ed.save();
+
 		var save_link = $('.save_button'); 
         var link = save_link.attr("href");
            
@@ -78,12 +85,18 @@
             interval_id = 0;
     });
 
-    // When the editor iFrame gets blur or focus
+    // Since TinyMCE editor opens in an iframe, only the above logic cannot be used to check
+    // whether the page is in view or not. Because whenever the editor is focus the iframe is in focus
+    // and as a result $(window) goes out of focus
+    // Since tinyMCE editor does not provide anything like ed.onfocus() I had to write some other way to find 
+    // whethet the iframe is in the focus
+    
     $(window).load(function(){
 
        var ed = tinymce.activeEditor;
-       console.log($(ed.getDoc()).contents().find('body'));
 
+       // ed.getDoc() gets the iframe of the editor
+       // contents() is used to refer to the contents of the iframe
        $(ed.getDoc()).contents().find('body').focus(function(){
            if (!interval_id)
                 interval_id = setInterval(saveForm, saveTimePeriod);
